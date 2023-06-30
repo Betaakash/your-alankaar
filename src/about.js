a9b5db09f974c7db6982417341524b2cbd5faf6b
Aakash <aakash@sharma.com>
2023-07-01 00:54:56 +0530

    okok css

---
+++ src/about.js
@@ -0,0 +1,13 @@
+import react from 'react'
+
+const   About = () => {
+    return (
+      <div>
+        <h1>About Page</h1>
+        <p>This is the About page content.</p>
+        {/* Add more content as needed */}
+      </div>
+    );
+  };
+  
+  export default About;
